bb4e68eb2d446cdfa577d0ca9cc1a96ccf1aa430
koukiAIT <kouki3820@gmail.com>
2021-12-09 16:45:43 +0900

    追加

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from diaries.DiarySample import DiarySample
 from diaries.K20014Diary import K20014Diary
+from diaries.K20047Diary import K20047Diary
+
 
 # のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
     K20014Diary(),
-    K20014Diary()
+    K20014Diary(),
+    K20047Diary(),
 ]
 
 for d in diaries:
