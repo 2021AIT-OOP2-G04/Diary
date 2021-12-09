98c60b362a0f3e13619a550ee01d47d80e29b810
kazuma59112 <253089294a@gmail.com>
2021-12-09 16:29:33 +0900

    mainの追加忘れてました。後藤

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from diaries.DiarySample import DiarySample
 from diaries.K20014Diary import K20014Diary
+from diaries.K20054Diary import K20054Diary
 
 # のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
     K20014Diary(),
+    K20054Diary(),
 ]
 
 for d in diaries:
