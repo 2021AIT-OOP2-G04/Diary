93ac9b3435f8631a19259db25319e9f3b440959c
takumi-hbk <78167997+takumi-hbk@users.noreply.github.com>
2021-12-15 15:09:20 +0900

    Diary内のコードの修正

--- diaries/OnzawaDiary.py
+++ diaries/OnzawaDiary.py
@@ -3,7 +3,7 @@ from diaries.AbstractDiary import AbstractDiary
 class OnzawaDiary(AbstractDiary):
 
 
-    def get_data(self):
+    def get_date(self):
         return "2021-12-09"
 
     def get_summary(self):
